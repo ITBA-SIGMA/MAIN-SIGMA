967d5fa26c47d83afd73ee376f496a268274b9e9
Yevhen <evgen_doroshenko25@ukr.net>
2021-10-15 23:26:18 +0300

    added some adaptation and mobile menu

---
+++ workFolder/week02/main-page/src/components/page-blocks/mobileNav/_mobileNav.js
@@ -0,0 +1,8 @@
+function CollNav() {
+    var x = document.getElementById("navList");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
