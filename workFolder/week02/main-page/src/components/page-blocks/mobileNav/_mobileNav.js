8dc874073097000aaebc557a19ff9e64eddba4da
atkach <nrfxfd@gmail.com>
2021-10-18 18:15:34 +0300

    added grid items

--- workFolder/week02/main-page/src/components/page-blocks/mobileNav/_mobileNav.js
+++ workFolder/week02/main-page/src/components/page-blocks/mobileNav/_mobileNav.js
@@ -1,8 +1,9 @@
+//mobile menu button
 function CollNav() {
-    var x = document.getElementById("navList");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
+  var x = document.getElementById("navList");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
   }
+}
