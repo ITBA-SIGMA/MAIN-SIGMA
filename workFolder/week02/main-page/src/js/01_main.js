e2e9853281afa10227bb6326f7c42bb4bab1edf0
mbuhel <bugelmax@gmail.com>
2021-11-14 12:08:00 +0200

    added script for animation portfolio

--- workFolder/week02/main-page/src/js/01_main.js
+++ workFolder/week02/main-page/src/js/01_main.js
@@ -2,5 +2,34 @@ $(function() {
     $('.burger').on('click', function(){
         $(this).parent().find('.menu').toggleClass('active');
         $(this).toggleClass('active');
-    })
+    });
+    showCategory(categoryList, categoryContent);
+
 });
+
+const categoryList = $('.category span');
+const categoryContent = $('.category-content .grid-item');
+
+function showCategory (categoryList, categoryContent){
+	categoryList.on('click', function(e){
+    const currentCategory = e.target.className;
+	    categoryContent.each(function(){
+	    	if(currentCategory === 'all'){
+	    		$(categoryContent).show();
+	    	} else if($(this).hasClass(currentCategory)){
+	    		$(this).show();
+	    	} else {
+	    		$(this).hide();
+	    	}
+		})
+		masonry();
+	});
+}
+
+function masonry() {
+	$('.category-content').masonry({
+	  // options
+	  itemSelector: '.grid-item',
+	  columnWidth: 280
+	});
+}
